5f5754bb4b61fbaf48cefba0655ed7deace3f3ef
Leosousa-dev <leonardocasa790@gmail.com>
2024-04-28 15:13:03 -0300

    chore: update logo file

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,10 +1,10 @@
 import {HeroUI, Headline, About} from "./ui"
-
+import logo from "../../assets/logo.png"
 
 const Hero = () => {
     return(
         <HeroUI>
-            <img src="../../../public/logo.png" alt="" />
+            <img src={logo} alt="" />
             <Headline>Lojinha da Dan</Headline>
             <About>
                 👋 Bem-Vindo a loja da Dandan assim que finalizar seu pedido entraremos em contato para realizar a entrega
