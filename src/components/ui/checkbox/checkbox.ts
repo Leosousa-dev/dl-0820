00c6313b2790f2ebad70aa2d4116bae55c83caa2
Leosousa-dev <leonardocasa790@gmail.com>
2025-03-17 19:35:03 -0300

    feat: add checkbox

---
+++ src/components/ui/checkbox/checkbox.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { theme } from "../../../theme/ui";
+export const Checkbox = styled.input`
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 7px;
+    border: 1px solid ${theme.colors.forgGray};
+    transition: all 0.2s;
+
+    &:checked {
+        background-color: ${theme.colors.express};
+        border-color: ${theme.colors.express};
+        background-image: url("data:image/svg+xml,%3Csvg width='12' height='10' viewBox='0 0 10 8' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1 4L3.5 6.5L8.5 1.5' stroke='white' stroke-width='2'/%3E%3C/svg%3E");
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+`;
